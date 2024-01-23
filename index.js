44ee3829cd76ded97185631ed702428fa06c39ca
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-23 12:04:36 +0100

    add console log

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ import { server } from "./server.js";
 
 const port = 8000;
 
-server.listen(port, () => {});
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
